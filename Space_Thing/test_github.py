28467e127b2530ba7a5962d2d7a1f2f741b46379
hogfig <57804817+hogfig@users.noreply.github.com>
2020-02-21 16:43:05 +0100

    Update test_github.py

--- Space_Thing/test_github.py
+++ Space_Thing/test_github.py
@@ -3,10 +3,7 @@ print('bella donna')
 x = 'Leo je gej'
 y = 'Marko je kralj'
 
-<<<<<<< HEAD
 print('smthing smthing light side')
 print (x,z,y)
-=======
 print (x,y)
 g= 'smthing smthing dark side'
->>>>>>> 1b07ef94c2607f7ee74576662898a658bc4f26f6
